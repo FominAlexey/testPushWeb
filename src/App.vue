2a6162881bcaabe93fd402f59284fded76950cf0
Aleksey <lekha.fomin2013@yandex.ru>
2023-03-20 17:13:33 +0300

    Change UI

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>Token: {{ token }}</div>
+  <div>Token Device: {{ token }}</div>
+  <button v-on:click="getToken()">Get TOKEN!</button>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view />
-  <button v-on:click="getToken()">Get TOKEN!</button>
 </template>
 <script>
 export default {
