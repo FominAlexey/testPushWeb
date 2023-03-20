be682e16cca0276c49f24dc918b70e91aa83ae93
Aleksey <lekha.fomin2013@yandex.ru>
2023-03-20 15:23:36 +0300

    1

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -6,7 +6,7 @@ importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-messaging.js');
 // Initialize the Firebase app in the service worker by passing in the messagingSenderId.
 firebase.initializeApp({
-    'messagingSenderId': '225449216286' // 4. Get Firebase Configuration
+    'messagingSenderId': '599087481711' // 4. Get Firebase Configuration
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background messages.
